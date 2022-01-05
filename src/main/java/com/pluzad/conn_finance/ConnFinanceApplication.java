cd334d764c95d8fe3284e6b855330bedadb08621
Sérgio Peters <sergiopeters@pluzad.com>
2022-01-05 13:07:31 -0300

    project created

---
+++ src/main/java/com/pluzad/conn_finance/ConnFinanceApplication.java
@@ -0,0 +1,13 @@
+package com.pluzad.conn_finance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnFinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnFinanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pluzad/conn_finance/ConnFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluzad.conn_finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnFinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
