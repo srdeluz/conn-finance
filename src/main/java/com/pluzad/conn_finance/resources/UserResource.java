2246b2a511dc95239492df9f807e166dedc2735f
Sérgio Peters <sergiopeters@pluzad.com>
2022-01-06 12:24:52 -0300

    Service, comp Registration

--- src/main/java/com/pluzad/conn_finance/resources/UserResource.java
+++ src/main/java/com/pluzad/conn_finance/resources/UserResource.java
@@ -1,21 +1,37 @@
 package com.pluzad.conn_finance.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pluzad.conn_finance.entities.User;
+import com.pluzad.conn_finance.services.UserServices;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+
+	@Autowired
+	private UserServices services;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User u = new User(1L, "Maria", "maria@maria.com", "19999999", "vaicatarcoco");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll() {		
+		List<User> list = services.findAll();		
+		return ResponseEntity.ok().body(list);
 	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = services.findById(id);		
+		return ResponseEntity.ok().body(obj);		
+	}
+	
 	
 	
 }
